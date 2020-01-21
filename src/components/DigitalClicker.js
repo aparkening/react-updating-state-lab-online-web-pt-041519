e2f4786e943f918b5f79a9f60a618c045927bcd9
aparkening <aaron.parkening@gmail.com>
2020-01-21 17:30:24 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+  // Set initial state
+  constructor(props) {
+    super()
+	  this.state = {
+	    timesClicked: 0
+	  }
+  }
+  // Change color to #333
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+            {this.state.timesClicked}
+        </button>
+      </div>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,62 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+  // Set initial state
+  constructor(props) {
+    super()
+	  this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+	  }
+  }
+
+  // Change bitrate to 12
+  handleBitClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }      
+    })
+  }
+
+  // Change resolution to 720p
+  handleResClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: '720p'
+        }
+      }      
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.settings.bitrate} - &nbsp;
+        <button 
+          className="bitrate"
+          onClick={this.handleBitClick}
+        >
+          Update bitrate
+        </button>
+        <br />
+        {this.state.settings.video.resolution} - &nbsp;
+        <button 
+          className="resolution"
+          onClick={this.handleResClick}
+        >
+          Update resolution
+        </button>
+      </div>
+    )
+  }
+}
